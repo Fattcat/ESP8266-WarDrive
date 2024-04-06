7b4ff3fd679731d7c6301155b38ff15fc8e72a15
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-04-06 08:53:30 +0200

    Update ESP8266-Wardriver.ino

--- ESP8266-Wardriver.ino
+++ ESP8266-Wardriver.ino
@@ -1,92 +1,99 @@
 #include <ESP8266WiFi.h>
 #include <SD.h>
 
-// Súbor na SD karte pre ukladanie údajov o skenovaní
-const char* scanFileName = "/Scan.txt";
+#define LED_PIN 2  // Pin pre LED diódu
+#define SCAN_INTERVAL 2000  // Interval skenovania v milisekundách
+#define BLINK_DURATION 300  // Trvanie blikania v milisekundách
+
+File dataFile;
+
+
+
+// ------------ CONNECTION ------------
+// NodeMCU ESP8266   ->    SD Card modul
+//
+// D5 (GPIO14)       ->    CLK
+// D6 (GPIO12)       ->    MISO
+// D7 (GPIO13)       ->    MOSI
+// D8 (GPIO15)       ->    CS
+// 3V3               ->    VCC
+// GND               ->    GND
+// PIN CS JE NA "D1 Mini ESP8266 3.3V" SD Card module na pine D8 a teda to je pin na NodeMCU D4
+// ------------ CONNECTION ------------
+
+
+
 
 void setup() {
   Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  
+  // Pripojenie k WiFi sieti
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
 
-  // Inicializácia SD karty
-  if (SD.begin(D1)) {
-    Serial.println("SD karta inicializovaná.");
-  } else {
-    Serial.println("Chyba inicializácie SD karty.");
+  Serial.println("Inicializácia SD karty...");
+  if (!SD.begin(4)) {
+    Serial.println("Chyba inicializácie SD karty!");
     return;
   }
 
-  // Pripojenie k Wi-Fi
-  connectToWiFi();
-
-  // Spustenie cyklu skenovania a ukladania dát
-  while (true) {
-    scanAndSave();
-    delay(6000);  // Čakanie 6 sekúnd pred ďalším skenovaním
-  }
+  Serial.println("SD karta je inicializovaná.");
 }
 
 void loop() {
-  // Loop nie je používaný, všetko sa deje v setup().
+  // Skenovanie Wi-Fi sietí
+  int networksFound = WiFi.scanNetworks();
+  if (networksFound == 0) {
+    Serial.println("Žiadne Wi-Fi siete neboli nájdené.");
+  } else {
+    Serial.print(networksFound);
+    Serial.println(" Wi-Fi sietí bolo nájdených.");
+
+    // Zapnutie zelenej LED diódy
+    digitalWrite(LED_PIN, HIGH);
+    delay(BLINK_DURATION);
+    digitalWrite(LED_PIN, LOW);
+
+    // Otvorenie alebo vytvorenie súboru ScanData.txt
+    dataFile = SD.open("ScanData.txt", FILE_WRITE);
+    if (dataFile) {
+      for (int i = 0; i < networksFound; ++i) {
+        // Zapisovanie údajov do súboru
+        dataFile.println("----------------------------------------------");
+        dataFile.print("SSID : ");
+        dataFile.println(WiFi.SSID(i));
+        dataFile.print("MAC : ");
+        dataFile.println(WiFi.BSSIDstr(i));
+        dataFile.print("CH : ");
+        dataFile.println(WiFi.channel(i));
+        dataFile.print("Sec Type : ");
+        dataFile.println(getEncryptionType(WiFi.encryptionType(i)));
+        dataFile.print("Signal Strenght : ");
+        dataFile.println(WiFi.RSSI(i));
+        dataFile.println("----------------------------------------------\n");
+      }
+      dataFile.close();
+    } else {
+      Serial.println("Chyba pri otváraní súboru.");
+    }
+  }
+
+  // Čakanie pred ďalším skenovaním
+  delay(SCAN_INTERVAL);
 }
 
-void connectToWiFi() {
-  Serial.print("Pripájam sa k Wi-Fi sieti");
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("\nPripojené k Wi-Fi sieti");
-}
-
-void scanAndSave() {
-  Serial.println("Spúšťam skenovanie...");
-
-  // Skenovanie dostupných sietí Wi-Fi
-  int networkCount = WiFi.scanNetworks();
-  if (networkCount == 0) {
-    Serial.println("Nenašli sa žiadne dostupné siete.");
-    return;
-  }
-
-  // Otvorenie alebo vytvorenie súboru pre zápis
-  File file = SD.open(scanFileName, FILE_WRITE);
-  if (!file) {
-    Serial.println("Chyba otvorenia súboru.");
-    return;
-  }
-
-  // Ukladanie údajov o sietiach do súboru
-  for (int i = 0; i < networkCount; ++i) {
-    file.println("-----------------------------------------------------");
-    file.println("SSID : " + WiFi.SSID(i));
-    file.println("MAC : " + WiFi.BSSIDstr(i));
-    file.println("Channel : " + String(WiFi.channel(i)));
-    file.println("Security Type : " + getEncryptionType(WiFi.encryptionType(i)));
-    file.println("Signal Strenght (dB) : " + String(WiFi.RSSI(i)));
-    file.println("-----------------------------------------------------");
-    file.println();
-  }
-
-  // Zatvorenie súboru
-  file.close();
-
-  Serial.println("Dáta boli uložené do súboru " + String(scanFileName));
-}
-
-String getEncryptionType(int authType) {
-  switch (authType) {
-    case ENC_TYPE_NONE:
-      return "None";
+String getEncryptionType(int encryptionType) {
+  switch(encryptionType) {
     case ENC_TYPE_WEP:
       return "WEP";
     case ENC_TYPE_TKIP:
-      return "TKIP";
+      return "WPA/TKIP";
     case ENC_TYPE_CCMP:
-      return "CCMP";
+      return "WPA2/AES";
+    case ENC_TYPE_NONE:
+      return "None";
     case ENC_TYPE_AUTO:
       return "Auto";
     default:
